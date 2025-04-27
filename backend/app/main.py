75f92994fe43da40dd5ece46c15cddb628f3a512
Daniel BN <daniel.batista.001@student.uni.lu>
2025-04-27 13:24:04 +0200

    Files moved and processfile endpoint added

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ import shutil
 from fastapi import FastAPI,File,UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 
+from scripts import csv_to_xes, xes_dfg
 
 app = FastAPI()
 
@@ -23,6 +24,14 @@ async def upload_file(file: UploadFile = File(...)):
         shutil.copyfileobj(file.file, buffer)
     return {"info": f"file '{file.filename}' saved"}
 
+
+@app.post("/processfile/")
+async def process_file(file:str):
+    upload_path = f"../data/uploads/{file}"
+    xes_path = f"../data/xes/{file.split(".")[0]}.xes"
+
+    csv_to_xes(upload_path, xes_path)
+    dfg = xes_dfg(xes_path)
 
 if __name__ == "__main__":
     uvicorn.run(app, host = "0.0.0.0",port=8000)
