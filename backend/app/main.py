26158e326761d36e7234c55e3906d991ef4d5a3b
Daniel BN <daniel.batista.001@student.uni.lu>
2025-05-22 21:22:48 +0200

    docker files added

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,11 +30,10 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
-origins = ["http://localhost:5173"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
